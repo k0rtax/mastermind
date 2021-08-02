68281e944a142bbba7226cd0dbde5fa1aadde163
dclol <dclol@DESKTOP-ROHHD24>
2021-08-02 16:52:01 +0200

    Add project files.

---
+++ HelloWorld/HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace HelloWorld
+{
+    public partial class MainWindow : Window
+    {
+        #region constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
