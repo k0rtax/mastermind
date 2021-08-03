edc3a5a9f10ffdd8cd7f15bf1f805d2915774f30
k0rtax <88333910+k0rtax@users.noreply.github.com>
2021-08-03 08:17:03 +0200

    Update Constants.cs

--- MastermindLibrary/MastermindLibrary/Constants.cs
+++ MastermindLibrary/MastermindLibrary/Constants.cs
@@ -4,7 +4,7 @@
     {
         #region fields
 
-        public const string MissmatchError = "missmatch on numbers of secrets and guesses";
+        public const string MismatchError = "mismatch on numbers of secrets and guesses";
 
         public const string NoInputError = "one or both inputs are missing";
 
